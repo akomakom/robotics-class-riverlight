0bd5f30d5df22cefbd0c047e07ee74d4f6cc6552
Akom <akom@akomlat>
2022-03-13 16:28:09 -0400

    minor refactoring

--- reference/tank-avoid/tank-avoid.ino
+++ reference/tank-avoid/tank-avoid.ino
@@ -1,8 +1,8 @@
 /*
- keyestudio Mini Tank Robot v2.0
- lesson 11
- ultrasonic_avoid_tank
- http://www.keyestudio.com
+  keyestudio Mini Tank Robot v2.0
+  lesson 11
+  ultrasonic_avoid_tank
+  http://www.keyestudio.com
 */
 int random2;
 int forward_distance;
@@ -17,7 +17,9 @@ int right_distance;
 #define MR_PWM 3    //define PWM control pin of right motor
 
 // driving constants
-#define TURN_TIME 500 // time it takes to make a turn
+#define TURN_TIME 500 // time it takes to make an obstacle-avoiding turn
+#define DISTANCE_OBSTRUCTION 50
+
 
 // ultrasonic server constants
 #define ANGLE_STRAIGHT 90
@@ -33,52 +35,52 @@ int pulsewidth;
 /************the function to run motor**************/
 void Car_forward()
 {
-  digitalWrite(MR_Ctrl,LOW);
-  analogWrite(MR_PWM,200);
-  digitalWrite(ML_Ctrl,LOW);
-  analogWrite(ML_PWM,245);
+  digitalWrite(MR_Ctrl, LOW);
+  analogWrite(MR_PWM, 200);
+  digitalWrite(ML_Ctrl, LOW);
+  analogWrite(ML_PWM, 240);
   display("FW");
 }
 void Car_back()
 {
-  digitalWrite(MR_Ctrl,HIGH);
-  analogWrite(MR_PWM,200);
-  digitalWrite(ML_Ctrl,HIGH);
-  analogWrite(ML_PWM,200);
+  digitalWrite(MR_Ctrl, HIGH);
+  analogWrite(MR_PWM, 200);
+  digitalWrite(ML_Ctrl, HIGH);
+  analogWrite(ML_PWM, 200);
   display("RV");
 }
 void Car_left()
 {
-  digitalWrite(MR_Ctrl,LOW);
-  analogWrite(MR_PWM,255);
-  digitalWrite(ML_Ctrl,HIGH);
-  analogWrite(ML_PWM,255);
+  digitalWrite(MR_Ctrl, LOW);
+  analogWrite(MR_PWM, 255);
+  digitalWrite(ML_Ctrl, HIGH);
+  analogWrite(ML_PWM, 255);
   display(">");
 }
 void Car_right()
 {
-  digitalWrite(MR_Ctrl,HIGH);
-  analogWrite(MR_PWM,255);
-  digitalWrite(ML_Ctrl,LOW);
-  analogWrite(ML_PWM,255);
+  digitalWrite(MR_Ctrl, HIGH);
+  analogWrite(MR_PWM, 255);
+  digitalWrite(ML_Ctrl, LOW);
+  analogWrite(ML_PWM, 255);
   display("<");
 }
 void Car_Stop()
 {
-  digitalWrite(MR_Ctrl,LOW);
-  analogWrite(MR_PWM,0);
-  digitalWrite(ML_Ctrl,LOW);
-  analogWrite(ML_PWM,0);
+  digitalWrite(MR_Ctrl, LOW);
+  analogWrite(MR_PWM, 0);
+  digitalWrite(ML_Ctrl, LOW);
+  analogWrite(ML_PWM, 0);
   display("X");
 }
 
 //The function to control servo
-void set_server_angle(int myangle) {
+void set_servo_angle(int myangle) {
   for (int i = 0; i <= 50; i = i + (1)) {
     pulsewidth = myangle * 11 + 500;
-    digitalWrite(servoPin,HIGH);
+    digitalWrite(servoPin, HIGH);
     delayMicroseconds(pulsewidth);
-    digitalWrite(servoPin,LOW);
+    digitalWrite(servoPin, LOW);
     delay((20 - pulsewidth / 1000));
   }
 }
@@ -90,14 +92,27 @@ float checkdistance() {
   delayMicroseconds(10);
   digitalWrite(Trig, LOW);
   float distance = pulseIn(Echo, HIGH) / 58.00;  //58.20, that is, 2*29.1=58.2
-  delay(10);
   return distance;
 }
-  //****************************************************************
-void setup(){
+
+/**
+ * Repeat distance measurement several times, average the result
+ */
+float checkdistance_average() {
+  int result = 0;
+  // the data will be more accurate if ultrasonic sensor detect a few times.
+  for (int j = 0; j < 10; j++) {
+    result += checkdistance();
+    delay(10);
+  }
+  return result / 10;
+}
+
+//****************************************************************
+void setup() {
   pinMode(servoPin, OUTPUT);
-  set_server_angle(ANGLE_STRAIGHT); //set servo to 90°
-  
+  set_servo_angle(ANGLE_STRAIGHT); //set servo to 90°
+
   pinMode(Trig, OUTPUT);
   pinMode(Echo, INPUT);
   pinMode(ML_Ctrl, OUTPUT);
@@ -107,66 +122,57 @@ void setup(){
 
   matrix_setup();
 }
-void loop(){
+void loop() {
   random2 = random(1, 100);
   forward_distance = checkdistance();  //assign the front distance detected by ultrasonic sensor to variable a
-  
-  if (forward_distance < 40) //when the front distance detected is less than 20 
+
+  if (forward_distance < 40) //when the front distance detected is less than 20
   {
-      Car_Stop();  //robot stops
-      delay(300); //delay in 500ms
-      set_server_angle(ANGLE_LEFT);  //Ultrasonic platform turns left
-      for (int j = 1; j <= 10; j = j + (1)) { //for statement, the data will be more accurate if ultrasonic sensor detect a few times.
-        left_distance = checkdistance();  //assign the left distance detected by ultrasonic sensor to variable left_distance
-      }
-      display(String(left_distance));
-      Serial.println(left_distance);
-      delay(500);
-      set_server_angle(ANGLE_RIGHT); //Ultrasonic platform turns right
-      for (int k = 1; k <= 10; k = k + (1)) {
-        right_distance = checkdistance(); //assign the right distance detected by ultrasonic sensor to variable right_distance
-      }
-      display(String(right_distance)); 
-      Serial.println(right_distance); 
-      
-      if (left_distance < 50 || right_distance < 50)  //robot will turn to the longer distance side when left or right distance is less than 50cm. 
+    Car_Stop();  //robot stops
+    delay(300); //delay in 500ms
+
+    set_servo_angle(ANGLE_LEFT);  //Ultrasonic platform turns left
+    left_distance = checkdistance_average();  //assign the left distance detected by ultrasonic sensor to variable left_distance
+    display(String(left_distance).substring(0, 3));
+    Serial.println(left_distance);
+    delay(300);
+    set_servo_angle(ANGLE_RIGHT); //Ultrasonic platform turns right
+    right_distance = checkdistance_average(); //assign the right distance detected by ultrasonic sensor to variable right_distance
+    display(String(right_distance).substring(0, 3));
+    Serial.println(right_distance);
+    delay(300);
+    set_servo_angle(ANGLE_STRAIGHT);  //Ultrasonic platform turns back to right ahead
+
+    if (left_distance < 50 || right_distance < 50)  //robot will turn to the longer distance side when left or right distance is less than 50cm.
+    {
+      if (left_distance > right_distance) //left distance is greater than right side
       {
-        if (left_distance > right_distance) //left distance is greater than right side      
-        {
-          set_server_angle(ANGLE_STRAIGHT);  //Ultrasonic platform turns back to right ahead         
-Car_left();  //robot turns left
-          delay(TURN_TIME);  //turn left for 500ms
-          Car_forward(); //go front
-        } 
-        else 
-        {
-          set_server_angle(ANGLE_STRAIGHT);
-          Car_right(); //robot turns right
-          delay(TURN_TIME);
-          Car_forward();  //go front
-        }
-      } 
-      else  //If both side is greater than or equal to 50cm, turn left or right randomly
+        Car_left();  //robot turns left
+      }
+      else
       {
-        display("?"); // random direction
-        if ((long) (random2) % (long) (2) == 0)  //When the random number is even
-        {
-          set_server_angle(ANGLE_STRAIGHT);
-          Car_left(); //tank robot turns left
-          delay(TURN_TIME);
-          Car_forward(); //go front
-        } 
-        else 
-        {
-          set_server_angle(ANGLE_STRAIGHT);
-          Car_right(); //robot turns right
-          delay(TURN_TIME);
-          Car_forward(); //go front
-       }
-     }
-  } 
+        Car_right(); //robot turns right
+      }
+    }
+    else  //If both side is greater than or equal to 50cm, turn left or right randomly
+    {
+      display("?"); // random direction, both sides seem open
+      if ((long) (random2) % (long) (2) == 0)  //When the random number is even
+      {
+        Car_left(); //tank robot turns left
+      }
+      else
+      {
+        Car_right(); //robot turns right
+      }
+    }
+
+    delay(TURN_TIME);  // Let motors run this long to turn
+    Car_forward(); //go front
+
+  }
   else  //If the front distance is greater than or equal to 20cm, robot car will go front
   {
-      Car_forward(); //go front
+    Car_forward(); //go front
   }
 }
