0f9415b6cef8bdbe283569364a4c34bb4d9340d1
Akom <regs@akom.net>
2022-03-17 14:56:24 -0400

    Cleaner voltage logic

--- reference/tank-voltage-divider/tank-voltage-divider.ino
+++ reference/tank-voltage-divider/tank-voltage-divider.ino
@@ -1,26 +1,26 @@
 
+// two equal resistor divider connected to analog pin 3
+// outside leads to ground and Vbat
+// cuts 2 lipo cell voltage in half to <5V so arduino can handle it
+#define VOLTAGE_PIN A3
 
-int analogPin = A3; // potentiometer wiper (middle terminal) connected to analog pin 3
-                    // outside leads to ground and +5V
-int val = 0;  // variable to store the value read
-float voltageTotal = 0;
-float voltagePerCell = 0;
-float factor = 1.9; // voltage divider is 1/2, with some inaccuracy
-float cells = 2;
-
+#define VOLTAGE_CONVERSION_FACTOR 2.0 // voltage divider is 1/2, with some inaccuracy
+#define VOLTAGE_CELL_COUNT 2    // number of batteries in series
 
 void setup() {
   Serial.begin(9600);           //  setup serial
 }
 
 void loop() {
-  val = analogRead(analogPin);  // read the input pin
-  voltageTotal = val/1024.0*5.0*factor;
-  voltagePerCell = voltageTotal / cells;
+  int val = analogRead(VOLTAGE_PIN);  // read the input pin
+  float voltageTotal = val/1024.0*5.0*VOLTAGE_CONVERSION_FACTOR;
+  float voltagePerCell = voltageTotal / VOLTAGE_CELL_COUNT;
+
   Serial.print(val);          // debug value
   Serial.print(" -> ");
   Serial.print(voltagePerCell);
   Serial.print(" -> ");
   Serial.println(voltageTotal);
+
   delay(500);
 }
